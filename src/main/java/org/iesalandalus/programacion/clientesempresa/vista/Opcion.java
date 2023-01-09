ad13f38e12c44d9cf6c73fedf4bcfc9705b5beee
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2023-01-09 02:57:39 +0100

    Opcion

---
+++ src/main/java/org/iesalandalus/programacion/clientesempresa/vista/Opcion.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.clientesempresa.vista;
+
+public enum Opcion {
+	BORRAR_CLIENTE,
+	BUSCAR_CLIENTE,
+	INSERTAR_CLIENTE,
+	MOSTRAR_CLIENTES,
+	MOSTRAR_CLIENTES_FECHA,
+	SALIR
+}
